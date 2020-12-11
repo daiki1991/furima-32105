28d534935ac867ec32464200053c6fb001f0d882
daiki1991 <73939379+daiki1991@users.noreply.github.com>
2020-12-11 23:52:35 +0900

    アソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  #has_one :purchase
+
   validates :text, presence: true
   validates :item_name, presence: true 
   validates :price, presence: true 
