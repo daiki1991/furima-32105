f01ad3696405136bef55efa0a56ac7c7304d858e
daiki1991 <73939379+daiki1991@users.noreply.github.com>
2020-12-11 23:31:47 +0900

    itemモデルバリデーション

---
+++ app/models/item.rb
@@ -0,0 +1,10 @@
+class Item < ApplicationRecord
+  validates :text, presence: true
+  validates :item_name, presence: true 
+  validates :price, presence: true 
+  validates :item_condition_id, presence: true 
+  validates :shipping_fee_id, presence: true 
+  validates :prefecture_id, presence: true 
+  validates :days_to_ship_id, presence: true 
+  validates :category_id, presence: true
+end
