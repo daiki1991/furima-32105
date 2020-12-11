75534f8d827de4ea162b467ed6c082ade3865a5c
daiki1991 <73939379+daiki1991@users.noreply.github.com>
2020-12-11 23:31:19 +0900

    createアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,17 +2,25 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
+    @items = Item.all
   end
 
-  #def new
-  #end
-
-  #def create
-  #end
+  def new
+    @item = Item.new 
+  end
 
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :text)
   end
 end
