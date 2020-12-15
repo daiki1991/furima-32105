b5f780fe018f8fecf335ce7aad8849702ef1a674
daiki1991 <73939379+daiki1991@users.noreply.github.com>
2020-12-15 21:56:23 +0900

    テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    item_name             {"商品名"}
+    price                 {300}
+    item_condition_id     {2}
+    shipping_fee_id       {2}          
+    prefecture_id         {2}
+    days_to_ship_id       {2}
+    category_id           {2}         
+    text                  {"商品です"}   
+    image                 {""}
+    user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/image.png'), filename: 'image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+   @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    it "商品名が空では登録できない" do
+      @item.item_name = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Item name can't be blank"
+    end
+
+   it "商品の説明が空では登録できない" do
+    @item.text = ""
+    @item.valid?
+    expect(@item.errors.full_messages).to include "Text can't be blank"
+    end
+
+    it "カテゴリーが空では登録できない" do
+      @item.category_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Category can't be blank"
+    end
+
+    it "商品の状態が空では登録できない" do
+      @item.item_condition_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Item condition can't be blank"
+    end
+ 
+    it "配送料の負担が空では登録できない" do
+      @item.shipping_fee_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Shipping fee can't be blank"
+    end
+
+    it "発送元地域が空では登録できない" do
+      @item.prefecture_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Prefecture can't be blank"
+    end
+
+    it "発送までの日数が空では登録できない" do
+      @item.days_to_ship_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Days to ship can't be blank"
+    end
+
+    it "価格が空では登録できない" do
+      @item.price = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Price can't be blank"
+    end
+
+    it "画像が空では登録できない" do
+      @item.image = []
+      expect(@item.errors.full_messages).to include 
+
+    end
+  end
 end
